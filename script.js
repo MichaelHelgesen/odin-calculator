24d06a3d892d397e091a6e7ed8a4518e5c4ab7d3
Michael Helgesen <michaelhelgesen02@gmail.com>
2024-03-13 10:23:12 +0100

    Make buttons run relevant functions on mouse click
    All the buttons are now connected to corresponding functions. Numbers
    and operators are saved to a variable and displayed on screen, while the
    "C" button clears them.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var firstNum = null;
+var lastNum = null;
+var operator = null;
+var inputString = "0";
+var screenEl = document.querySelector(".screen");
+var buttons = document.querySelectorAll("button");
 var add = function (num1, num2) {
     return num1 + num2;
 };
@@ -10,9 +16,6 @@ var divide = function (num1, num2) {
 var multiply = function (num1, num2) {
     return num1 * num2;
 };
-var firstNum = null;
-var lastNum = null;
-var operator = null;
 var operate = function (operator, num1, num2) {
     switch (operator) {
         case ("+"):
@@ -29,3 +32,35 @@ var operate = function (operator, num1, num2) {
             break;
     }
 };
+var saveValue = function (value) {
+    if (!firstNum) {
+        firstNum = value;
+        inputString = value;
+    }
+    else {
+        inputString += value;
+    }
+    screenEl.textContent = inputString;
+};
+var clearValues = function () {
+    inputString = "0";
+    firstNum = null;
+    screenEl.textContent = inputString;
+};
+var registerInput = function (input) {
+    switch (input.target.textContent) {
+        case "C":
+            clearValues();
+            break;
+        case "=":
+            sum();
+            break;
+        default:
+            saveValue(input.target.textContent);
+            break;
+    }
+};
+var sum = function () {
+    console.log("sum");
+};
+buttons.forEach((function (btn) { return (btn).addEventListener("click", function (e) { return registerInput(e); }); }));
--- script.ts
+++ script.ts
@@ -1,3 +1,10 @@
+let firstNum: string = null;
+let lastNum: number = null;
+let operator: string = null;
+let inputString: string = "0";
+const screenEl: HTMLElement = document.querySelector(".screen");
+const buttons = document.querySelectorAll("button");
+
 const add = function(num1: number, num2: number): number {
 	return num1 + num2;
 };
@@ -10,10 +17,6 @@ const divide = function(num1: number, num2: number):number {
 const multiply = function(num1:number, num2: number): number {
 	return num1 * num2;
 }
-let firstNum: number = null;
-let lastNum: number = null;
-let operator: string = null;
-
 const operate = function(operator: string, num1: number, num2: number): number {
 	switch(operator) {
 		case("+"): return add(num1, num2);
@@ -27,6 +30,38 @@ const operate = function(operator: string, num1: number, num2: number): number {
 	}
 }
 
+const saveValue = function(value: string): void {
+	if(!firstNum) {
+		firstNum = value;
+		inputString = value;
+	} else {
+		inputString += value;
+	}
+	screenEl.textContent = inputString;
+}
+
+const clearValues = function(): void {
+	inputString = "0";
+	firstNum = null;
+	screenEl.textContent = inputString;
+}
+
+const registerInput = function (input): void {
+	switch(input.target.textContent) {
+		case "C": clearValues(); 
+		break;
+		case "=": sum();
+		break;
+		default: saveValue(input.target.textContent); 
+		break;
+	}
+}
+
+const sum = function(): void {
+	console.log("sum");
+}
+
+buttons.forEach((btn => (btn).addEventListener("click", (e) => registerInput(e) )));
 
 
 
